a1cf201083dae7717fe8939f0520740a28ff339d
kahil nayton <kahilnayton@gmail.com>
2021-04-05 22:43:38 -0400

    refactored with sound

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,54 @@
+import { useState } from 'react';
+
 import Head from 'next/head';
-// import { Link } from 'next/link';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
+import { beforeRunStretches } from '../components/beforeRunStretches';
+import { afterRunStretches } from '../components/afterRunStretches';
 // import styled from 'styled-components';
 // import BeforeRun from '../components/BeforeRun';
 import Stretch from '../components/Stretch';
+import {
+  Container,
+  ImageWrapper,
+  Button,
+  StretchWrapper,
+  ButtonContainer,
+} from '../styles/layout';
 
 export default function Home() {
+  const [selectedStretch, setSelectedStretch] = useState(null);
   return (
     <div className={styles.container}>
       <Head>
         <title>Stretchhh</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Stretch />
+      {!selectedStretch && (
+        <ButtonContainer>
+          <Button
+            className="before"
+            onClick={() => setSelectedStretch(beforeRunStretches)}
+          >
+            Before Run
+          </Button>
+          <Button
+            className="after"
+            onClick={() => setSelectedStretch(afterRunStretches)}
+          >
+            After Run
+          </Button>
+        </ButtonContainer>
+      )}
+      {selectedStretch && (
+        <Stretch
+          selectedStretch={selectedStretch}
+          setSelectedStretch={setSelectedStretch}
+        />
+      )}
+      {/* <Button className="change" onClick={() => setSelectedStretch({})}>
+        Change stretch
+      </Button> */}
     </div>
   );
 }
